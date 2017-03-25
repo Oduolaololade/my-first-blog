a53cf570ab990dcd4e665ab08198ae93ccd1a5d3
Oduolaololade <pweteyloulhy@yahoo.com>
2017-03-25 15:17:57 +0100

    My Django girls app, first commit

---
+++ lolade.py
@@ -0,0 +1,10 @@
+print("Hello miss ololade")
+
+#these are if statements
+lolade = "slimmer" 
+if lolade is "fatter than Fatima":
+	print ("Fat lolade")
+elif lolade is "slimmer than Fatima":
+	print ("Fat Fatima")
+else:
+	print("Slim Lolade")
